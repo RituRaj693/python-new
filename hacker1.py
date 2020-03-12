14cc029aa7bed5d532dba6bd18e34d7aa3d6eab8
Ritu Raj <rituraj@Ritus-MacBook-Air.local>
2020-03-12 22:56:13 +0530

    practice

--- hacker1.py
+++ hacker1.py
@@ -4,3 +4,19 @@ l[5]='k'
 print(l)
 string=''.join(l)
 print(string)'''
+
+'''string='abracadabra'
+string=string[:5]+'k'+ string[6:]
+print(string)'''
+
+
+string='abracadabra'
+l=list(string)
+print(l)
+index=int(input('enter the index'))
+for i in l:
+    l[index]='k'
+string=''.join(l)
+print(string)
+
+
